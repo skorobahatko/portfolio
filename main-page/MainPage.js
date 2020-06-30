d564dd0e3ecbfe592d55cbbfa181d011a0e46869
skorobahatko <maxim.skorobagatko@gmail.com>
2020-06-30 14:14:42 +0300

    added second page with projects + anchors

--- main-page/MainPage.js
+++ main-page/MainPage.js
@@ -1,5 +1,22 @@
+let projectUl = document.getElementById('projectUL');
+let projectsArr = [
+    {
+        url: 'https://github.com/skorobahatko/movie-app-0.1',
+        name: 'test movie site by moviedb api'
+    },
+    {
+        url: '#',
+        name: 'its all now('
+    }
+];
 
-let showSecondPage = function () {
-    let show2 = document.getElementById('showSecondPage');
-    show2.classList.add('hide');
-};
+for (let x of projectsArr) {
+    let a = document.createElement('a');
+    a.href = x.url;
+    a.innerText = x.name;
+    let li = document.createElement('li');
+    li.classList.add('project-li');
+    li.appendChild(a);
+    projectUl.appendChild(li);
+}
+
