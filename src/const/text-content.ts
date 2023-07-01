3b1782f7bb2e8a5ba7942759f3436fcc04a9cd7f
skorobahatko <maxim.skorobagatko@gmail.com>
2023-07-01 17:54:06 +0300

    added text-content file

---
+++ src/const/text-content.ts
@@ -0,0 +1,30 @@
+export const MAIN_CONTENT_HEADER_FOOTER = {
+  HEADER_NAV_ABOUT: 'About',
+  HEADER_NAV_PORTFOLIO: 'Portfolio',
+  FOOTER_COPYRIGHTS: '© 2023 Maksym Skorobahatko'
+};
+
+export const ABOUT_PAGE_CONTENT = {
+  HEADER_WELCOME: 'Welcome.',
+  HEADER_NAMING: 'My name is Lorem ipsum',
+  HEADER_SUB_NAMING:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae harum iure illum error delectus impedit! Tenetur, numquam veritatis blanditiis alias mollitia totam doloribus accusantium ex, maxime iste hic, excepturi eos!',
+  MAIN_CONTENT_1:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum delectus beatae eius illum corporis laudantium ipsam molestiae inventore nisi. Pariatur maiores molestias deleniti quis odio suscipit mollitia voluptatem itaque aperiam!',
+  MAIN_CONTENT_2:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem provident fugiat nesciunt labore vitae, odio eius deleniti dolor inventore, libero, in similique praesentium beatae! Minus dolore quae nesciunt cum alias?',
+  WORK_EXPERIENCE_ARRAY: [
+    {
+      fromTo: 'August 2022 - Present',
+      title: 'Daizy',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis sed neque perspiciatis velit unde quia, impedit laboriosam, praesentium magnam soluta blanditiis autem? Similique optio, veniam eligendi quae est doloribus aliquam?'
+    },
+    {
+      fromTo: 'October 2020 - August 2022',
+      title: '42-Flows',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis sed neque perspiciatis velit unde quia, impedit laboriosam, praesentium magnam soluta blanditiis autem? Similique optio, veniam eligendi quae est doloribus aliquam?'
+    }
+  ]
+};
