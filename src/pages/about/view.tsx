29a8e031d55d080277d30509a5da792b84877fef
skorobahatko <maxim.skorobagatko@gmail.com>
2023-07-01 17:54:44 +0300

    added changes into about page

--- src/pages/about/view.tsx
+++ src/pages/about/view.tsx
@@ -1,29 +1,45 @@
 import { Fragment } from 'react';
 import { BodyComponent } from '../../components';
+import { ABOUT_PAGE_CONTENT } from '../../const';
 import './styles.scss';
+
+const AboutWorkExperience = ({ fromTo, title, content }: { fromTo: string; title: string; content: string }) => {
+  return (
+    <div className="about-work-experience-content-container">
+      <div className="time-from-to">
+        <span>{fromTo}</span>
+      </div>
+      <div className="content-container">
+        <h3 className="content-title">{title}</h3>
+        <br />
+        <p className="content-data">{content}</p>
+      </div>
+    </div>
+  );
+};
 
 const AboutPageContent = () => {
   return (
     <section className="about-content-container">
       <div className="main-header-title">
-        <h1 className='name-title'>
-          Welcome.
+        <h1 className="name-title">
+          {ABOUT_PAGE_CONTENT.HEADER_WELCOME}
           <br />
-          My name is Lorem ipsum
+          {ABOUT_PAGE_CONTENT.HEADER_NAMING}
         </h1>
-        <span className='position-title'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae harum iure illum error delectus impedit! Tenetur, numquam veritatis blanditiis alias
-          mollitia totam doloribus accusantium ex, maxime iste hic, excepturi eos!
-          </span>
+        <span className="position-title">{ABOUT_PAGE_CONTENT.HEADER_SUB_NAMING}</span>
       </div>
       <div className="main-info-content">
-        <p className='main-content-text'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum delectus beatae eius illum corporis laudantium ipsam molestiae inventore nisi.
-          Pariatur maiores molestias deleniti quis odio suscipit mollitia voluptatem itaque aperiam!
+        <p className="main-content-text">
+          {ABOUT_PAGE_CONTENT.MAIN_CONTENT_1}
           <br />
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem provident fugiat nesciunt labore vitae, odio eius deleniti dolor inventore,
-          libero, in similique praesentium beatae! Minus dolore quae nesciunt cum alias?
+          {ABOUT_PAGE_CONTENT.MAIN_CONTENT_2}
         </p>
+      </div>
+      <div className="main-work-experience-container">
+        {ABOUT_PAGE_CONTENT.WORK_EXPERIENCE_ARRAY.map(({ fromTo, title, content }) => (
+          <AboutWorkExperience fromTo={fromTo} title={title} content={content} />
+        ))}
       </div>
     </section>
   );
